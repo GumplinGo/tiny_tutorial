d353cf498f98ebe0be973f50131b0253d99b0a8e
guojianglin <1448373124@qq.com>
2020-08-29 20:43:22 +0800

    3.3 webpack配置实践

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import './style.css'
 class progress {
   // percentTimer;
   // progressBarTimer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MinniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'none',
@@ -7,4 +10,59 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    port: 3000,
+    contentBase: './dist',  // 起服务的地址(即定位到我们的输出文件地址)
+    open: true,              // 自动打开浏览器
+    compress: true,         // gzip压缩
+    hot: true,   // 开启热更新
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',//模板文件地址
+      filename: 'index.html',//指定打包后的文件名字
+    }),
+    new MinniCssExtractPlugin({
+      //指定输出的文件名
+      filename: 'main.css'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MinniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 50 * 1024,
+              loader: 'file-loader',
+              esModule: false,
+              outputPath: 'public/imgs',
+              name: '[name].[ext]'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                '@babel/preset-env'
+            ],
+            plugins:[
+              ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+            ]
+          }
+        }
+      },
+    ]
+  }
 };
